4d78c292402c3839ca9a99d9165cd0a66b4e198b
vineela <vineela19.kanaparthi@gmail.com>
2026-06-04 01:53:28 +0530

    Hello world created

---
+++ 05-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "variables passed to the script: $@"
+echo "no.of variables passed to the script:$#"
+echo "script name: $0"
+echo "home directory of the user who is running this script: $HOME"
+echo "user name who is running this script: $USEER"
+echo "PID of this current script: $$"
+echo "present working directory: $PWD"
+echo " previous command response is $?"
+echo "PID of last command running in background: $!"
