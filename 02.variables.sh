23c0bc9d71fced27576c0f5751ac8ba69df9d6d4
vineela <vineela19.kanaparthi@gmail.com>
2026-06-04 01:31:40 +0530

    Hello world created

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "Enter first person name"
-read person1
-echo "Enter second person name"
-read person2
+person1=$(date)
+person2=$(pwd)
+
  echo "$person1 and $person2 are good friends"
