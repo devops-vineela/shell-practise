cadaaede4cb43a6c059d692f7de0dfa5d94c75b6
vineela <vineela19.kanaparthi@gmail.com>
2026-06-04 03:02:26 +0530

    Hello world created

---
+++ 10-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+userid=$(id -u)
+mkdir -p /var/log/shell-script-logs
+log_folder="/var/log/shell-script-logs"
+script-name=$(echo $0 |cut -d "." -f1)
+log-file=$log_folder/$script-name.log
+
+if [ $userid -ne 0 ]
+then 
+  echo "$R Error: $N you should run this script with root access" &>>$log-file
+  exit 1
+else
+  echo "$G you are running with root access $N" &>>$log-file
+fi
