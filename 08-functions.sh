656b01c6ce0e7fba2881042ed2d327869642b2b1
vineela <vineela19.kanaparthi@gmail.com>
2026-06-04 02:26:20 +0530

    Hello world created

---
+++ 08-functions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+validate(){
+    if [ $1 -ne 0 ]
+    then
+      echo "$2 is failure"
+      exit 1
+    else
+      echo "$2 is success"
+    fi
+}
+
+dnf install nginx -y
+validate $? "installing nginx"
