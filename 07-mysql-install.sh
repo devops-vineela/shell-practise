bd3ebb4deb4d474ab66d0085ceaed7e193346f17
vineela <vineela19.kanaparthi@gmail.com>
2026-06-04 02:12:40 +0530

    Hello world created

--- 07-mysql-install.sh
+++ 07-mysql-install.sh
@@ -3,6 +3,15 @@ user=$(id -u)
 if [ $user -ne 0 ]
 then 
   echo "you should run this script with root access"
+  exit 1
 else
   echo "you are running with root access"
+fi
+
+dnf install mysql -y
+if [ $? -ne 0 ]
+then
+  echo "installing mysql is failure"
+else 
+  echo "installing mysql is success"
 fi
