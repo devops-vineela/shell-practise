43279ff8f07f2ff92a61d14b088cf9ee864fc319
vineela <vineela19.kanaparthi@gmail.com>
2026-06-05 15:16:53 +0530

    roboshop project started

--- script-1.sh
+++ script-1.sh
@@ -2,6 +2,6 @@
 echo " Today we are going to setup MongoDB for our application"
 GREETING=Good-Morning
 echo "$GREETING, Welcome to the MongoDB setup for our application"
-./script-2.sh
+source ./script-2.sh
 
 
