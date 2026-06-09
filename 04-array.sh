c0b56cc849a367232068c112ee39e98f1b12f0cb
vineela <vineela19.kanaparthi@gmail.com>
2026-06-09 11:59:38 +0530

    roboshop project started

--- 04-array.sh
+++ 04-array.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+failure(){
+    echo "Error occurred at line $1 while executing: $2"
+}
+trap 'failure ${LINE_NO} {BASH_COMMAND}' ERR
 movies=(court bahubali pushpa)
 echoooo "${moviessss[0]}"
 echo "all movies are: ${movies[@]}"
